b3923d25d253e8539d464260323253b85fe1294a
Himath_R <himath4510@gmail.com>
2023-05-20 20:36:33 +1000

    test

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="font-general-regular flex flex-col justify-center items-center mt-4 mb-4">
-      ✨ Made by Himath ✨ :D
+      ✨ Made by Himath! ✨ :D
     </div>
   );
 };
