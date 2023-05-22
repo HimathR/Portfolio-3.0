ff0ed6dbae835ba086bb65d95882d16e631e643b
Himath_R <himath4510@gmail.com>
2023-05-23 01:26:23 +1000

    modal fix update 1

--- app/experience/CardGrid.tsx
+++ app/experience/CardGrid.tsx
@@ -4,12 +4,105 @@ import ExperienceModal from "./ExperienceModal";
 export interface CardData {
   imgSrc: string;
   title: string;
-  description: string;
+  subtitle: string;
+  links: string[];
   company: string;
   position: string;
   tools: string[];
   technologies: string[];
+  date: string;
+  description: string[];
 }
+
+const cardData: CardData[] = [
+  {
+    imgSrc:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Atlassian-logo.svg/1200px-Atlassian-logo.svg.png",
+    title: "Atlassian",
+    subtitle: "Software Engineering Intern",
+    date: "November 2022 - February 2023",
+    links: [
+      "https://i.gyazo.com/89204fbb187bffa331bd75117521565c.png",
+      "https://i.gyazo.com/eee8c9246b1a12bfa787fa264fcbf72d.png",
+    ],
+    company:
+      "Atlassian is a leading provider of collaboration, development, and issue tracking software for teams",
+    position: "Software Engineering Intern",
+    tools: ["Jira Service Management"],
+    technologies: ["UI development", "Filtering for linked Jira tickets"],
+    description: [
+      "For my intern project, I feature led a new UI for a highly suggested JSM feature that adds filtering for linked Jira tickets to various assets objects",
+      "Project is in production and is being used by thousands of JSM customers worldwide (approx. 20000 customers at time of project completion)",
+      "Completed project ahead of schedule and joined the team in contributing to sprint work",
+    ],
+  },
+  {
+    imgSrc:
+      "https://assets-global.website-files.com/636e894daa9e99940a604aef/6385b3d7140115fbf69f92db_SwyftX%20Logo.svg",
+    title: "Swyftx",
+    subtitle: "Software Engineering Intern",
+    date: "June 2022 - July 2022",
+    links: [
+      "https://i.gyazo.com/89204fbb187bffa331bd75117521565c.png",
+      "https://i.gyazo.com/eee8c9246b1a12bfa787fa264fcbf72d.png",
+    ],
+    company: "Swyftx is Australia's 2nd largest cryptocurrency exchange",
+    position: "Software Engineering Intern",
+    tools: ["Flask", "SQLAlchemy", "React", "TypeScript"],
+    technologies: [
+      "Database schema design",
+      "REST API",
+      "Event booking system",
+      "Slackbot integration",
+    ],
+    description: [
+      "Worked with 4 other interns to create SwyftSocial: an all-in-one event booking and management system app that could be used as an internal tool within the company",
+      "Liased with a number of key stakeholders within the company including employees from the People and Culture, Security, Design/UI, and R&D teams.",
+      "Designed a database schema and REST API backend using Flask and SQLAlchemy to interact with the React Typescript frontend",
+      "Integrated extended features to the app, including .ics Calendar invites, .csv file exports for events, and integration with a Slackbot to notify Swyftx employees when events have been created or cancelled",
+    ],
+  },
+  {
+    imgSrc: "https://i.gyazo.com/4a917009cacfced912e985e4c446095a.png",
+    title: "Cinefly",
+    subtitle: "Software Engineering Intern",
+    date: "July 2022 - October 2022",
+    links: [
+      "https://i.gyazo.com/89204fbb187bffa331bd75117521565c.png",
+      "https://i.gyazo.com/eee8c9246b1a12bfa787fa264fcbf72d.png",
+    ],
+    company:
+      "Cinefly is a company focused on creating user generated content seamlessly",
+    position: "Software Engineering Intern",
+    tools: ["React", "Redux"],
+    technologies: ["Rendering", "UI design", "Modals", "Styling"],
+    description: [
+      "Implemented rendering functionality with React and Redux for the company's video editing platform",
+      "Assisted with creation of new modals, styling, UI design and debugging",
+    ],
+  },
+  {
+    imgSrc:
+      "https://www.mellimited.com/wp-content/uploads/2019/02/mel_logo.png",
+    title: "ME Learning",
+    subtitle: "Software Development Intern",
+    date: "November 2021 - January 2022",
+    links: [
+      "https://i.gyazo.com/89204fbb187bffa331bd75117521565c.png",
+      "https://i.gyazo.com/eee8c9246b1a12bfa787fa264fcbf72d.png",
+    ],
+    company:
+      "Online internship with an online education company based in Osaka, Japan",
+    position: "Software Development Intern",
+    tools: ["Google TTS API", "NLP/T5 Transformers"],
+    technologies: ["Audio file generation", "Question generation"],
+    description: [
+      "Utilised Google's TTS API to create programs to automate generation of over 12000 audio files for English/Japanese words from various dictionaries",
+      "Applied NLP/T5 Transformers knowledge to generate MCQ, Fill in The Blank, Anagram, and Unscramble questions when given the script for a story book",
+      "Proliferated question generation program within the company to eliminate the need for manual comprehension question creation for the company's E-Book Library. 2000+ tailor-made questions for 500+ books can be processed in 30 minutes with the Python program.",
+    ],
+  },
+];
 
 interface CardProps extends CardData {
   handleClick: (card: CardData) => void;
@@ -42,48 +135,6 @@ const CardGrid: React.FC = () => {
     setCurrentCard(card);
     setIsOpen(true);
   };
-
-  const cardData = [
-    {
-      imgSrc:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Atlassian-logo.svg/1200px-Atlassian-logo.svg.png",
-      title: "Card 1",
-      description: "Card 1 Description",
-      company: "Company 1",
-      position: "Position 1",
-      tools: ["Tool 1", "Tool 2"],
-      technologies: ["Tech 1", "Tech 2"],
-    },
-    {
-      imgSrc:
-        "https://assets-global.website-files.com/636e894daa9e99940a604aef/6385b3d7140115fbf69f92db_SwyftX%20Logo.svg",
-      title: "Card 2",
-      description: "Card 1 Description",
-      company: "Company 1",
-      position: "Position 1",
-      tools: ["Tool 1", "Tool 2"],
-      technologies: ["Tech 1", "Tech 2"],
-    },
-    {
-      imgSrc: "https://i.gyazo.com/4a917009cacfced912e985e4c446095a.png",
-      title: "Card 3",
-      description: "Card 1 Description",
-      company: "Company 1",
-      position: "Position 1",
-      tools: ["Tool 1", "Tool 2"],
-      technologies: ["Tech 1", "Tech 2"],
-    },
-    {
-      imgSrc:
-        "https://www.mellimited.com/wp-content/uploads/2019/02/mel_logo.png",
-      title: "Card 4",
-      description: "Card 1 Description",
-      company: "Company 1",
-      position: "Position 1",
-      tools: ["Tool 1", "Tool 2"],
-      technologies: ["Tech 1", "Tech 2"],
-    },
-  ];
 
   return (
     <div className="flex flex-wrap justify-center">
--- app/experience/ExperienceModal.tsx
+++ app/experience/ExperienceModal.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { CardData } from "./CardGrid";
 
 type ModalProps = {
@@ -11,30 +12,153 @@ const ExperienceModal: React.FC<ModalProps> = ({
   cardDetails,
   onClose,
 }) => {
+  const [carouselIndex, setCarouselIndex] = useState(0);
+
   if (!showModal || !cardDetails) {
     return null;
   }
+  const {
+    imgSrc,
+    title,
+    subtitle,
+    description,
+    company,
+    position,
+    tools,
+    technologies,
+    date,
+    links,
+  } = cardDetails;
+
+  const handleClickOutside = (event: React.MouseEvent) => {
+    if ((event.target as Element).classList.contains("modal")) {
+      onClose();
+    }
+  };
+
+  const handleCarouselClick = () => {
+    setCarouselIndex((carouselIndex + 1) % links.length);
+  };
 
   return (
     <div
-      className={`fixed z-50 left-0 top-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 ${
+      onClick={handleClickOutside}
+      className={`modal fixed z-50 left-0 top-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-75 ${
         showModal ? "modalIn" : "modalOut"
       }`}
     >
-      <div className="bg-white rounded-xl p-4 max-w-2xl m-4 text-black">
-        <button onClick={onClose} className="float-right">
-          Close
+      <div className="bg-gray-800 text-white rounded-lg overflow-hidden shadow-lg w-3/4 h-3/4 md:max-w-6xl m-4 md:m-0">
+        <div className="grid grid-cols-3 h-full">
+          <div className="col-span-1 p-6 space-y-8">
+            <div className="mb-4">
+              <img
+                className="w-full h-full object-contain row-span-1"
+                src={imgSrc}
+                alt={title}
+              />
+            </div>
+
+            <h4 className="text-lg font-semibold"> {company}</h4>
+
+            <div className="mb-4">
+              <h4 className="text-lg font-semibold">Time Of Employment</h4>
+              <h3>{date}</h3>
+            </div>
+
+            <div className="mb-4">
+              <h4 className="text-lg font-semibold">Tools</h4>
+              <ul className="flex flex-wrap">
+                {tools.map((tool, index) => (
+                  <li
+                    key={index}
+                    className="m-1 px-2 py-1 text-sm bg-gray-700 rounded-full hover:bg-gray-600"
+                  >
+                    {tool}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="mb-4">
+              <h4 className="text-lg font-semibold">Technologies</h4>
+              <ul className="flex flex-wrap">
+                {technologies.map((tech, index) => (
+                  <li
+                    key={index}
+                    className="m-1 px-2 py-1 text-sm bg-gray-700 rounded-full hover:bg-gray-600"
+                  >
+                    {tech}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="col-span-2 grid grid-rows-3 p-6">
+            {links && links.length > 0 && (
+              <div className="relative w-full h-120 mb-6 rounded-md overflow-hidden">
+                <img
+                  className="w-full h-full object-contain cursor-pointer"
+                  src={links[carouselIndex]}
+                  alt={`carousel image ${carouselIndex}`}
+                  onClick={handleCarouselClick}
+                />
+
+                {/* Left and right navigation arrows */}
+                <div
+                  className="absolute top-1/2 left-3 transform -translate-y-1/2 cursor-pointer text-xl text-white"
+                  onClick={(e) => {
+                    e.stopPropagation(); // Prevent the image click handler from being triggered
+                    setCarouselIndex(
+                      (carouselIndex - 1 + links.length) % links.length
+                    );
+                  }}
+                >
+                  &#8592;
+                </div>
+                <div
+                  className="absolute top-1/2 right-3 transform -translate-y-1/2 cursor-pointer text-xl text-white"
+                  onClick={(e) => {
+                    e.stopPropagation(); // Prevent the image click handler from being triggered
+                    setCarouselIndex((carouselIndex + 1) % links.length);
+                  }}
+                >
+                  &#8594;
+                </div>
+
+                {/* Dots indicating the current image */}
+                <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 space-x-2 flex items-center">
+                  {links.map((link, index) => (
+                    <div
+                      className={`w-2 h-2 rounded-full ${
+                        carouselIndex === index ? "bg-white" : "bg-gray-500"
+                      }`}
+                      key={index}
+                    />
+                  ))}
+                </div>
+              </div>
+            )}
+            <div className="row-span-2">
+              <h1 className="text-3xl font-bold">{title}</h1>
+              <h2 className="text-2xl font-semibold">{subtitle}</h2>
+              <div>
+                <h4 className="text-lg font-semibold">Tasks: </h4>
+                <ul>
+                  {description.map((desc, index) => (
+                    <li key={index} className="text-sm">
+                      - {desc}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button
+          onClick={onClose}
+          className="text-white bg-gray-700 p-2 mt-4 rounded-lg text-center btn-glow"
+        >
+          Close Modal
         </button>
-        <h2 className="font-sohne text-36pt font-bold leading-[0.9] tracking-tighter  mt-4">
-          {cardDetails.title}
-        </h2>
-        <p className="mt-2">{cardDetails.description}</p>
-        <p className="mt-2">Company: {cardDetails.company}</p>
-        <p className="mt-2">Position: {cardDetails.position}</p>
-        <p className="mt-2">Tools: {cardDetails.tools.join(", ")}</p>
-        <p className="mt-2">
-          Technologies: {cardDetails.technologies.join(", ")}
-        </p>
       </div>
     </div>
   );
